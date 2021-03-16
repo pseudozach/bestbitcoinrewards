ca6ecb9493d0f9f51f651e145e2b7ab655491cb9
pseudozach <git@pseudozach.com>
2021-03-15 17:53:39 -0700

    comparison table

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <div>
+      <img alt="logo" src="./assets/percentage.png">
+      <h1>Best Bitcoin Rewards</h1>
+      <p>Comparison of all Bitcoin cashback rewards sites and apps. Searchable and (mostly) up to date.</p>
+    </div>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Table/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Table from './components/table.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Table
   }
 }
 </script>
